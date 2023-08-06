de0fc9556ae446dc40977861f377c3f6375fd21d
thelastjeremiah <thelastgaviola@gmail.com>
2023-08-06 10:39:32 +0800

    arrange css

--- components/ListOfTasks.vue
+++ components/ListOfTasks.vue
@@ -18,7 +18,7 @@
 </template>
 
 <script>
-import { mapState, mapMutations } from 'vuex'
+import { mapMutations } from 'vuex'
 export default {
   props: ['task'],
   methods: {
@@ -33,103 +33,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-.grid-text-and-icon{
-  display: grid;
-  grid-template-columns: repeat(2,1fr);
-  align-content: center;
-  align-items: center;
-}
-.list-of-task-container-parent {
-  margin-left: 2rem;
-  margin-right: 2rem;
-  padding-bottom: 0.3rem;
-  margin-top: 0.3rem;
-}
-
-.block{
-  display: block;
-}
-.font-awesome-trash-icon-red:hover {
-  color: #ff5c46 !important;
-  cursor: pointer !important;
-}
-
-.list-of-task-container-child {
-  margin-top: 0.3rem;
-  margin-bottom: 0.3rem;
-  display: flex;
-  align-content: center;
-  align-items: center;
-  border: 1px solid #1a1d1c4b;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-  border-radius: 0.3125rem;
-}
-
-.list-of-task-container-child:hover {
-  background-color: #ededed;
-}
-
-.list-of-task-container-child:hover .font-awesome-trash-icon-red{
-  color: #ededed;
-}
-
-.list-of-taks-false{
-  color:black
-}
-
-.list-status-false{
-  font-weight: bold;
-}
-.list-of-taks-false:hover{
-  cursor: pointer;
-}
-
-.list-of-taks-true{
-  color:gray
-}
-
-.list-of-taks-true:hover{
-  cursor: pointer;
-}
-
-
-.check-color-false{
-  color:#469c7d;
-  font-size: 1.8rem;
-  padding-left: 1rem;
-  padding-right: 1rem;
-  padding-top:0.2rem;
-  padding-bottom: 0.2rem;
-}
-
-.check-color-false{
-  cursor: pointer;
-}
-
-.check-color-true{
-  color:grey;
-  font-size: 1.8rem;
-  padding-left: 1rem;
-  padding-right: 1rem;
-  padding-top:0.2rem;
-  padding-bottom: 0.2rem;
-}
-
-.check-color-true:hover{
-  cursor: pointer;
-}
-
-.check-color-false:hover{
-  color:#0d553b;
-}
-
-.font-awesome-trash-icon-red{
-  padding-left: 18rem;
-  padding-right: 1rem;
-  color:white;
-  font-size:1.9rem;
-}
-
-</style>
