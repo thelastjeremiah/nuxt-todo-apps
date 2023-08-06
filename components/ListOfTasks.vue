33a49a062529a0a8fa5cba3cf3811759633c1128
thelastjeremiah <thelastgaviola@gmail.com>
2023-08-06 10:01:56 +0800

    toggle bug fix

--- components/ListOfTasks.vue
+++ components/ListOfTasks.vue
@@ -2,8 +2,17 @@
   <div class="list-of-task-container-parent">
     <div class="list-of-task-container-child">
       <font-awesome-icon icon="circle-check"   :class="{'check-color-true': task.status, 'check-color-false': !task.status}" @click="toggleDone"/>
-      <p class="list-of-taks">{{ task.name }}</p>
-      <font-awesome-icon icon="trash" class="font-awesome-trash-icon-red"  @click="removeTask"/>
+      <div class="grid-text-and-icon">
+        <div class="block">
+          <p :class="{'list-of-taks-true': task.status, 'list-of-taks-false': !task.status}" @click="toggleDone">
+            <s v-if="task.status">{{ task.name }}</s>
+            <span v-else class="list-status-false">{{ task.name }}</span>
+          </p>
+        </div>
+        <div class="block">
+          <font-awesome-icon icon="trash" class="font-awesome-trash-icon-red"  @click="removeTask"/>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -12,12 +21,6 @@
 import { mapState, mapMutations } from 'vuex'
 export default {
   props: ['task'],
-  computed: {
-    ...mapState(['tasks']),
-    task() {
-      return this.tasks.find((task) => task.id === 1) || {}
-    },
-  },
   methods: {
     ...mapMutations(['REMOVE_TASK','TOGGLE_TASK']),
     toggleDone() {
@@ -32,6 +35,12 @@ export default {
 </script>
 
 <style scoped>
+.grid-text-and-icon{
+  display: grid;
+  grid-template-columns: repeat(2,1fr);
+  align-content: center;
+  align-items: center;
+}
 .list-of-task-container-parent {
   margin-left: 2rem;
   margin-right: 2rem;
@@ -39,16 +48,18 @@ export default {
   margin-top: 0.3rem;
 }
 
+.block{
+  display: block;
+}
 .font-awesome-trash-icon-red:hover {
   color: #ff5c46 !important;
   cursor: pointer !important;
 }
 
 .list-of-task-container-child {
-  margin-top: 0.3rem; /* There was a typo in your code, ',' should be '.' */
+  margin-top: 0.3rem;
   margin-bottom: 0.3rem;
   display: flex;
-  justify-content: space-between;
   align-content: center;
   align-items: center;
   border: 1px solid #1a1d1c4b;
@@ -64,6 +75,26 @@ export default {
   color: #ededed;
 }
 
+.list-of-taks-false{
+  color:black
+}
+
+.list-status-false{
+  font-weight: bold;
+}
+.list-of-taks-false:hover{
+  cursor: pointer;
+}
+
+.list-of-taks-true{
+  color:gray
+}
+
+.list-of-taks-true:hover{
+  cursor: pointer;
+}
+
+
 .check-color-false{
   color:#469c7d;
   font-size: 1.8rem;
@@ -73,6 +104,10 @@ export default {
   padding-bottom: 0.2rem;
 }
 
+.check-color-false{
+  cursor: pointer;
+}
+
 .check-color-true{
   color:grey;
   font-size: 1.8rem;
@@ -80,6 +115,10 @@ export default {
   padding-right: 1rem;
   padding-top:0.2rem;
   padding-bottom: 0.2rem;
+}
+
+.check-color-true:hover{
+  cursor: pointer;
 }
 
 .check-color-false:hover{
